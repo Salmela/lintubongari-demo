0a743edd04137fc01b3bd2343ece10172df1971c
Aleksi Salmela <aleksi.salmela@helsinki.fi>
2018-07-08 20:17:58 +0300

    Create recomendation dialog for entry boxes

--- src/entry.ts
+++ src/entry.ts
@@ -1,8 +1,28 @@
 import {bindable} from "aurelia-framework";
 
+const SHOW_MENU_CSS = "display: initial;";
+
 export class Entry {
 
 	@bindable placeholder = "";
 	@bindable value = "";
+
+	menuStyle = "";
+
+	menu = ["kotka", "harakka"];
+
+	focus() {
+		// show the menu
+		let newStyle = SHOW_MENU_CSS;
+
+		// the input size could be browser dependent so we set them here
+		newStyle += `top: ${this.input.offsetHeight - 1}px;`;
+		newStyle += `min-width: ${this.input.offsetWidth - 1}px;`;
+		this.menuStyle = newStyle;
+	}
+
+	blur() {
+		this.menuStyle = "";
+	}
 }
 
